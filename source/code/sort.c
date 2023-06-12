b70b30620c48d5dd87eb9d33d6f06374a8da05d2
swain <shenkun@bupt.edu.cn>
2023-06-12 22:35:37 +0800

    add sort

--- source/code/sort.c
+++ source/code/sort.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #define SWAP(arr, i, j)      \
     do {                     \
@@ -54,8 +55,35 @@ void sort_quick(int *arr, int left, int right)
 /* 归并排序 */
 void sort_merge(int *arr, int left, int right)
 {
-    // int mid = (right - left) / 2;
-    // arr[right - left];
+    printf("in %d %d\n", left, right);
+    if (left >= right - 1) {
+        return;
+    }
+    int merge[right - left];
+    int mergelen = 0;
+    int i;
+    int j;
+
+    printf("before\n");
+    prt(arr + left, right - left + 1);
+    int mid = (left + right) / 2;
+    sort_merge(arr, left, mid);
+    sort_merge(arr, mid + 1, right);
+    i = left;
+    j = mid + 1;
+    while (i <= mid && j <= right) {
+        merge[mergelen++] = arr[i] < arr[j] ? arr[i++] : arr[j++];
+    }
+    for (; i <= mid; i++) {
+        merge[mergelen++] = arr[i++];
+    }
+    for (; j <= right; j++) {
+        merge[mergelen++] = arr[j++];
+    }
+    printf(" %d --> %d\n", right - left + 1, mergelen);
+    memcpy(arr + left, merge, sizeof(int) * mergelen);
+    printf("after\n");
+    prt(arr + left, right - left + 1);
 }
 
 /* 辅助函数 */
@@ -71,7 +99,7 @@ void prt(int *arr, int arrlen)
 int main()
 {
     int arr[100] = {5, 9, 7, 10, 3, 1};
-    sort_quick(arr, 0, 5);
+    sort_merge(arr, 0, 5);
     prt(arr, 6);
 
     return 0;
